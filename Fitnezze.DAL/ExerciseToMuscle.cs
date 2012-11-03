ff4f8a2733f2748b162ce0f3e5197d305034fb8a
Denis Gridnev <denis.gridnev@gmail.com>
2012-11-03 22:10:17 +0400

    create solution, add dal

---
+++ Fitnezze.DAL/ExerciseToMuscle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+
+namespace Fitnezze.DAL
+{
+    [Table]
+    public class ExerciseToMuscle
+    {
+        [Association]
+        public int ExerciseId { get; set; }
+
+        [Association]
+        public int MuscleId { get; set; }
+    }
+}
---
+++ Fitnezze.DAL/Exerсise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+
+namespace Fitnezze.DAL
+{
+    [Table]
+    public class Exerсise
+    {
+        [Column(IsPrimaryKey = true, 
+            IsDbGenerated = true, 
+            DbType = "INT NOT NULL Identity", 
+            CanBeNull = false, 
+            AutoSync = AutoSync.OnInsert)]
+        public int Id { get; set; }
+
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public string Description { get; set; }
+
+        [Column]
+        public byte Image { get; set; }
+    }
+}
